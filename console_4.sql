d9dbb272160f834828d1ad9a54d58f442c1dd3ac
chris0512 <kyang3200@gmail.com>
2022-04-24 14:50:07 +0800

    Second Commit

--- console_4.sql
+++ console_4.sql
@@ -7,3 +7,5 @@ SELECT name, age FROM cats WHERE breed = 'Tabby';
 SELECT cat_id, age FROM cats WHERE cat_id = age;
 
 SELECT cat_id AS id , name FROM cats;
+
+SELECT name AS 'cat_name', breed AS 'kitty breed' FROM cats;
